a159454eea5974aeaee2af3e63c54e69d84420e0
Michael <emichael283@gmail.com>
2024-12-09 17:46:25 +0100

    Added script to generate all needed data for plotting

---
+++ adversarial/generate_plots_data.sh
@@ -0,0 +1,39 @@
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type float --execution-type float --adv-execution-type float
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type float --execution-type float --adv-execution-type quant
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type float --execution-type float --adv-execution-type transaxx
+
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type float --execution-type quant --adv-execution-type float
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type float --execution-type quant --adv-execution-type quant
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type float --execution-type quant --adv-execution-type transaxx
+
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type float --execution-type transaxx --adv-execution-type float
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type float --execution-type transaxx --adv-execution-type quant
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type float --execution-type transaxx --adv-execution-type transaxx
+
+
+
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type quant --execution-type float --adv-execution-type float
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type quant --execution-type float --adv-execution-type quant
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type quant --execution-type float --adv-execution-type transaxx
+
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type quant --execution-type quant --adv-execution-type float
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type quant --execution-type quant --adv-execution-type quant
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type quant --execution-type quant --adv-execution-type transaxx
+
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type quant --execution-type transaxx --adv-execution-type float
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type quant --execution-type transaxx --adv-execution-type quant
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type quant --execution-type transaxx --adv-execution-type transaxx
+
+
+
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type transaxx --execution-type float --adv-execution-type float
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type transaxx --execution-type float --adv-execution-type quant
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type transaxx --execution-type float --adv-execution-type transaxx
+
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type transaxx --execution-type quant --adv-execution-type float
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type transaxx --execution-type quant --adv-execution-type quant
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type transaxx --execution-type quant --adv-execution-type transaxx
+
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type transaxx --execution-type transaxx --adv-execution-type float
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type transaxx --execution-type transaxx --adv-execution-type quant
+python adversarial/resnet_attack_eval.py --prompt 0 --conv-axx-level-list 10 4 3 4 5 6 7 --param-execution-type transaxx --execution-type transaxx --adv-execution-type transaxx
